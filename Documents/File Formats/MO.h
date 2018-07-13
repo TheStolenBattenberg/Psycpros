49babee35865839e1a59753e9dc2e8966313901b
TheStolenBattenberg <thestolenbattenberg@gmail.com>
2018-07-13 22:41:03 +0100

    Update MO.h
    Added additional findings

--- Documents/File Formats/MO.h
+++ Documents/File Formats/MO.h
@@ -7,16 +7,24 @@
 #include "KFTypes.h"
 
 typedef struct {
-	uint  uFileEOF;
-	uint  unknown01;
-	uint  uTMDOffset;
-	uint  unknown02;
-	uint  unknown03;
-	uint  unknown04;
-} MO_HEADER;
+	uint  uFileEOF;             //Offset to the end of file.
+	uint  uNumAnim;             //Number of animations.
+	uint  uTMDOffset;           //Offset to the base mesh TMD.
+	uint  uUnknown;
+} MO_Header;
+
+typedef struct {
+	uint uAnimPtrsOffset; //Always 0x14, as the first animation is at 0x14.
+	uint *pAnimPtr;       //Depends on the value of uNumAnim.
+} MO_AnimHeader;
+
+typedef struct {
+	uint uNumFrame;
+	
+} MO_Anim;
+
 
 /** NOTES:
- *
- * 'unknown01' increments by 1, then unknown04 increments by 4.
- * 'unknown03' always appears to be '0x14000000'
+ *  That unknown int in the header is bugging me, but we won't know what it is until
+ *  we have discovered more of the file.
 **/
